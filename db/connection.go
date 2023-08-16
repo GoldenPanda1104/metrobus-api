b5b39d7759a4daf5bad201c584a33c9a53cb0be7
Juan Mendoza <jmendoza@ols360.com>
2023-08-16 09:08:04 -0500

    try to fix db connection

--- db/connection.go
+++ db/connection.go
@@ -1,15 +1,16 @@
 package db
 
 import (
+	"log"
+
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-	"log"
 )
 
 // DSN hace referencia a Database string
 
 // Remote DB
-var dsn = "host=ep-wispy-queen-49748303.us-east-2.aws.neon.tech user=fl0user password=9YwNQ3XVcsbA dbname=metrobus-dev port=5432"
+var dsn = "host=ep-misty-morning-21300543.us-east-2.aws.neon.tech user=fl0user password=BtYGCsu1aj6O dbname=metrobusdev port=5432 sslmode=require"
 
 // Local DB
 // var dsn = "host=localhost user=admin password=admin dbname=metrobus_dev port=5432"
