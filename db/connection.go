166b441e885d94eee9582d5d539a3efec6127473
Juan Mendoza <thejami180202@gmail.com>
2023-08-15 23:29:51 -0500

    temp(db):set remote db

--- db/connection.go
+++ db/connection.go
@@ -9,10 +9,10 @@ import (
 // DSN hace referencia a Database string
 
 // Remote DB
-// var dsn = "host=ep-wispy-queen-49748303.us-east-2.aws.neon.tech user=fl0user password=9YwNQ3XVcsbA dbname=metrobus-dev port=5432"
+var dsn = "host=ep-wispy-queen-49748303.us-east-2.aws.neon.tech user=fl0user password=9YwNQ3XVcsbA dbname=metrobus-dev port=5432"
 
 // Local DB
-var dsn = "host=localhost user=admin password=admin dbname=metrobus_dev port=5432"
+// var dsn = "host=localhost user=admin password=admin dbname=metrobus_dev port=5432"
 var DB *gorm.DB
 
 func DBConnection() {
